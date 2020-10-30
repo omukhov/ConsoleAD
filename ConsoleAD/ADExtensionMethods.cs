da8da529d5c95549a7cdb99f144841434b6d937f
artem <Artem.Omukhov@vittecon.onmicrosoft.com>
2020-10-30 13:42:16 +0400

    Add project files.

---
+++ ConsoleAD/ADExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.DirectoryServices;
+
+namespace ConsoleAD
+{
+    /// <summary>
+    /// Класс расширения метода доступа к Active Directory
+    /// </summary>
+    public static class ADExtensionMethods
+    {
+        /// <summary>
+        /// Получить значение свойства
+        /// </summary>
+        public static string GetPropertyValue(this SearchResult sr, string propertyName)
+        {
+            string ret = string.Empty;
+
+            if (sr.Properties[propertyName].Count > 0)
+                ret = sr.Properties[propertyName][0].ToString();
+
+            return ret;
+        }
+    }
+}
---
+++ ConsoleAD/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Diagnostics;
+using System;
+using System.DirectoryServices;
+
+namespace ConsoleAD
+{
+    class Program
+    {
+        /// <summary>
+        /// Ввод логина
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите логин: ");
+            string userName = Console.ReadLine();
+            Program p = new Program();
+            p.GetAUser(userName);            
+        }
+
+        /// <summary>
+        /// Запрос у сервера LDAP строки подключения
+        /// </summary>
+        private string GetCurrentDomainPath()
+        {
+            DirectoryEntry de = new DirectoryEntry("LDAP://RootDSE");
+
+            return "LDAP://" + de.Properties["defaultNamingContext"][0].ToString();
+
+        }
+
+        /// <summary>
+        /// Получить данные пользователя
+        /// </summary>
+        private DirectorySearcher GetAdditionalUserInfo(DirectoryEntry de)
+        {
+            DirectorySearcher ds = null;
+
+            ds = new DirectorySearcher(de);
+
+            /// <summary>
+            /// Полное имя пользователя
+            /// </summary>
+            ds.PropertiesToLoad.Add("name");
+
+            /// <summary>
+            /// Еmail Адрес пользователя
+            /// </summary>
+            ds.PropertiesToLoad.Add("mail");
+
+            /// <summary>
+            /// Имя пользователя
+            /// </summary>
+            ds.PropertiesToLoad.Add("givenname");
+
+            /// <summary>
+            /// Фамилия пользователя
+            /// </summary>
+            ds.PropertiesToLoad.Add("sn");
+
+            /// <summary>
+            /// Логин пользователя
+            /// </summary>
+            ds.PropertiesToLoad.Add("userPrincipalName");
+
+            /// <summary>
+            /// Уникальное имя - Полное имя + путь
+            /// </summary>
+            ds.PropertiesToLoad.Add("distinguishedName");
+
+            return ds;
+        }
+
+        /// <summary>
+        /// Получение одного пользователя
+        /// </summary>
+        public void GetAUser(string userName)
+        {
+            DirectorySearcher ds = null;
+            DirectoryEntry de = new DirectoryEntry(GetCurrentDomainPath());
+            SearchResult sr;
+
+            ds = GetAdditionalUserInfo(de);
+
+            /// <summary>
+            /// Установка фильтра для поиска конкретного пользователя
+            /// </summary>
+            ds.Filter = "(&(objectCategory=User)(objectClass=person)(userPrincipalName=" + userName + ")";
+
+            sr = ds.FindOne();
+
+            if (sr != null)
+            {
+                Debug.WriteLine(sr.GetPropertyValue("name"));
+                Debug.WriteLine(sr.GetPropertyValue("mail"));
+                Debug.WriteLine(sr.GetPropertyValue("givenname"));
+                Debug.WriteLine(sr.GetPropertyValue("sn"));
+                Debug.WriteLine(sr.GetPropertyValue("userPrincipalName"));
+                Debug.WriteLine(sr.GetPropertyValue("distinguishedName"));
+            }
+
+        }
+
+    }
+}
